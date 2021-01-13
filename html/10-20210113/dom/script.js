519f73c756a6a04f9805bd408f381aa7086724a6
namnh06 <namnh.developer@gmail.com>
2021-01-13 21:16:51 +0700

    Finished ss10

--- html/10-20210113/dom/script.js
+++ html/10-20210113/dom/script.js
@@ -2,3 +2,10 @@ function doiSoLop(lop) {
   document.getElementById('chu').innerHTML = 'Aptech PHP ' + lop;
   document.getElementById('chu').style.backgroundColor = 'yellow';
 }
+
+function xemAnh() {
+  var ten = document.getElementById('anh').src;
+  console.log(ten);
+  document.getElementById('anh').src =
+    'https://www.w3schools.com/js/pic_bulboff.gif';
+}
