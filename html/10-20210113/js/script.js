421eb1839d76adc2e0d6b9003ce0c0c388fc798c
namnh06 <namnh.developer@gmail.com>
2021-01-13 20:52:46 +0700

    WIP ss11

--- html/10-20210113/js/script.js
+++ html/10-20210113/js/script.js
@@ -4,4 +4,12 @@ function quetNha() {
   console.log('Quet');
   console.log('Lay ki');
   console.log('Do rac');
+  var a = 5;
+  var b = 6;
+  var c = a + b;
+  console.log(c);
+  var d = '9';
+  var e = a + b + d + c;
+  console.log(e);
+  a = 6;
 }
